1b77490cc50243bc411693a851ea81004abfbe3a
Dariana <ingrid.dariana.patino@gmail.com>
2016-03-14 16:45:14 +0000

    casi final

---
+++ css/main.cs
@@ -0,0 +1,126 @@
+@import url(http://fonts.googleapis.com/css?family=Lato:100,300,400,700);
+@import url(http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700);
+@import url(http://fonts.googleapis.com/css?family=Noto+Sans:400,700);
+html {
+  height: 100%; }
+
+body {
+  background: #ffffff;
+  font-weight: bold;
+  line-height: 2.0;
+  font-family: "Lato", sans-serif;
+  color: #00000; }
+
+.container a {
+  color: #A5732A; }
+  .container a:hover {
+    text-decoration: none;
+    color: #DAB074;
+    outline: none;
+    text-decoration: none; }
+  .container a:focus {
+    outline: none;
+    text-decoration: none;
+    outline-offset: 0; }
+.container h1, .container h2, .container #header h2, #header .container h2, .container h2, .container h3, .container h5, .container h6 {
+  font-weight: bold;
+  font-family: "Lato",sans-serif; }
+
+ul li {
+  list-style: none; }
+
+.container h1, .container h2, .container #header h2, #header .container h2 {
+  color: #686868; }
+.container h2 {
+  font-size: 22px; }
+.container h3 {
+  font-size: 18px; }
+
+.overflow {
+  overflow: hidden; }
+
+.btn-common {
+  border: 1px solid #DBB075;
+  font-size: 14px;
+  font-family: "Open Sans",sans-serif;
+  color: #797979;
+  font-weight: bolder;
+  padding: 10px 25px; }
+  .btn-common:hover {
+    box-shadow: none;
+    outline: none;
+    color: #D29948;
+    background: none; }
+
+.uppercase {
+  text-transform: uppercase; }
+
+#action .col-sms-5 {
+  position: absolute;
+  right: 0;
+  top: 0;
+  height: 100%; }
+
+.container .align-right {
+  text-align: right; }
+.container .inline {
+  display: inline-block; }
+
+.padding {
+  padding: 65px 0; }
+
+.padding-top {
+  padding-top: 90px; }
+
+.padding-bottom {
+  padding-bottom: 65px; }
+
+.padding-right {
+  padding-right: 80px; }
+
+.padding-left {
+  padding-left: 80px; }
+
+.margin-bottom {
+  margin-bottom: 35px; }
+
+#header {
+  margin-bottom: 0;
+  padding: 30px 0 60px 0; }
+  #header .navbar-inverse .container {
+    position: relative; }
+
+.social-icons ul li {
+  padding: 0 10px; }
+  .social-icons ul li:last-child {
+    padding-right: 0px; }
+  .social-icons ul li a {
+    font-size: 19px;
+    color: #D3D3D3;
+    padding: 0px; }
+    .social-icons ul li a:hover {
+      color: #DBB075;
+      background: none; }
+
+#header .navbar {
+  border: none;
+  margin-bottom: none;
+  background: #FFFFFF; }
+#header .navbar-toggle {
+  margin-top: 20px; }
+#header .navbar-brand {
+  padding: none;
+  margin-left: 0; }
+#header h1, #header h2 {
+  padding: none;
+  margin: none; }
+
+.navbar-nav.navbar-right > li a {
+  color: #00000;
+  padding: 20px;
+  text-transform: uppercase;
+  font-weight: bold; }
+.navbar-nav.navbar-right > li:last-child {
+  margin-right: 20px; }
+
+/*# sourceMappingURL=main.cs.map */
